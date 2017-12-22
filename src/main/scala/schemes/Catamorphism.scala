b6e1db389ad2a674d18e592f6398f66f7fc7fb6d
gianlo <gianlo@gmail.com>
2017-12-22 20:47:01 +0000

    example of tree created with coalg and ana and then reduction using cata

--- src/main/scala/schemes/Catamorphism.scala
+++ src/main/scala/schemes/Catamorphism.scala
@@ -11,3 +11,8 @@ object Catamorphism {
   def cata[F[_], A](alg: FAlgebra[F, A])(data: Fix[F])(implicit ff: Functor[F]): A =
     alg(ff.map(data.unFix)(xx => cata(alg)(xx)))
 }
+
+
+
+
+
--- src/main/scala/schemes/FAlgebra.scala
+++ src/main/scala/schemes/FAlgebra.scala
@@ -4,4 +4,5 @@ import scala.language.higherKinds
 
 object FAlgebra {
   type FAlgebra[F[_], A] = F[A] => A
+  type FCoalgebra[F[_], A] = A => F[A]
 }
